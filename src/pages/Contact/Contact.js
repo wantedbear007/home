986318d906a77f266c6d0841cbf5985265237681
Bhanupratap Singh <pratapsinghbhanu444@gmail.com>
2022-01-18 23:04:15 +0530

    Contact page completed with mobile responsivness.

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,10 +1,62 @@
 import React from "react";
 import "./Contact.css";
+import Fade from "react-reveal/Fade";
 
 const Contact = () => {
   return (
-    <div className="contactParentContainer">
-      <h1 className="contactText">Contact</h1>
+    <div className="privilegeClass">
+    <Fade bottom>
+      <div className="contactParentContainer">
+        <h1 className="text">Contact</h1>
+        <div className="contactContainers">
+          <div className="contactCard">
+            <img
+              src={require("../../assets/icons/linkedin.png")}
+              className="contactLogo"
+            />
+            <h2 className="contactText">LinkedIn</h2>
+            <button className="contactButton">Get In Touch</button>
+          </div>
+        </div>
+
+        <div className="contactContainers">
+          <div className="contactCard">
+            <img
+              src={require("../../assets/icons/gmail.png")}
+              className="contactLogo"
+            />
+            <h2 className="contactText">Gmail</h2>
+            <button className="contactButton">Drop a Mail</button>
+          </div>
+        </div>
+
+        <div className="contactContainers">
+          <div className="contactCard">
+            <img
+              src={require("../../assets/icons/telegram.png")}
+              className="contactLogo"
+            />
+            <h2 className="contactText">Telegram</h2>
+            <button className="contactButton">Have a chat</button>
+          </div>
+        </div>
+
+        <div className="contactContainers">
+          <div className="contactCard">
+            <img
+              src={require("../../assets/icons/github.png")}
+              className="contactLogo"
+            />
+            <h2 className="contactText">Github</h2>
+            <button className="contactButton">Peek My Work</button>
+          </div>
+        </div>
+        
+      </div>
+      <h2 className="credits">
+        Website proudly designed and developed by Bhanupratap with Love❤️.
+      </h2>
+    </Fade>
     </div>
   );
 };
